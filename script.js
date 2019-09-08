7c48c8b67484cda0eb75952b2a0a0495c834bb32
zoeferencova <zoeferencova@gmail.com>
2019-09-08 17:38:26 -0400

    made form input clear after submission

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ $(function addItemToList() {
 		e.preventDefault();
 		const inputValue = $('#js-shopping-list-form input').val();
 		$('.shopping-list').append(`<li><span class="shopping-item">${inputValue}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
+		$('#js-shopping-list-form input').val('');
 	});
 });
